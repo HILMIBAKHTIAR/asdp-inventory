8e700d078c8c6029476d8e8f5207e00e30d57d30
LAPTOP-JLB5HIDH\HILMI BAKHTIAR <hilmibakhtiar14@gmail.com>
2021-08-04 20:58:56 +0700

    crate model done button register done

---
+++ app/skb.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class skb extends Model
+{
+    //
+}
---
+++ app/verspm.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class verspm extends Model
+{
+    //
+}
