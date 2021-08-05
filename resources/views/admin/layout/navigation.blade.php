a935c1788c60dd5e44beed401d425b38f3571659
samcz <samcz646@gmail.com>
2021-08-05 10:38:19 +0700

    UI UX Nav done

--- resources/views/admin/layout/navigation.blade.php
+++ resources/views/admin/layout/navigation.blade.php
@@ -1,17 +1,23 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: rgb(56, 145, 166)">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{Route('admin.index')}}" style="margin-top: 40px; margin-bottom: 40px;">
+        <div class="sidebar-brand-icon">
+            {{-- <i class="fas fa-laugh-wink"></i> --}}
+            <img src="{{url('backend/img/asdp.svg')}}" alt="" style="width: 100%;">
         </div>
-        <div class="sidebar-brand-text mx-3">asdp</div>
+        {{-- <div class="sidebar-brand-text mx-3">asdp</div> --}}
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="{{Route('admin.index')}}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Pengadaan</span></a>
+    </li>
     <li class="nav-item active">
         <a class="nav-link" href="{{''}}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
