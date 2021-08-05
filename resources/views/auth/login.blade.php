c24b52617ae5916e510231c00ba4a811ea19f337
LAPTOP-JLB5HIDH\HILMI BAKHTIAR <hilmibakhtiar14@gmail.com>
2021-08-05 10:48:15 +0700

    ui login revisi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -112,9 +112,9 @@
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
+            
+            
+            <div style="padding-top: 45px; padding-left: 20px;" class="col-xl-10 col-lg-12 col-md-9">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
