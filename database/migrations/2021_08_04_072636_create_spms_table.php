41a3bf61946adf55567197abafb41f91ebab9354
Satria Teguh Perkasa <satriateguhghz@gmail.com>
2021-08-24 13:22:54 +0700

    SPM REVSI DONE

--- database/migrations/2021_08_04_072636_create_spms_table.php
+++ database/migrations/2021_08_04_072636_create_spms_table.php
@@ -16,7 +16,6 @@ class CreateSpmsTable extends Migration
         Schema::create('spms', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('mataanggaran_id');
             $table->unsignedBigInteger('ttd1');
             $table->unsignedBigInteger('ttd2');
             $table->unsignedBigInteger('ttd3');
@@ -30,14 +29,13 @@ class CreateSpmsTable extends Migration
             $table->date('tanggal');
             $table->date('tahun_anggaran');
             $table->string('jenis_transaksi');
-            $table->string('program');
+            $table->string('program')->nullable();
             $table->string('penerima_dana')->nullable();
             $table->string('nomor_rekening')->nullable();
             $table->string('bank')->nullable();
             $table->integer('nomor_surat_spm');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('mataanggaran_id')->references('id')->on('mataanggarans')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('ttd1')->references('id')->on('karyawans')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('ttd2')->references('id')->on('karyawans')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('ttd3')->references('id')->on('karyawans')->onDelete('cascade')->onUpdate('cascade');
