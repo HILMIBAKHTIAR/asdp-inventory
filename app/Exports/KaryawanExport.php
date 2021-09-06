f0c4f4073861f3b4f6540ee3217d6bd52a1aace8
Satria Teguh Perkasa <satriateguhghz@gmail.com>
2021-09-06 11:03:29 +0700

    custom Heading excel export

--- app/Exports/KaryawanExport.php
+++ app/Exports/KaryawanExport.php
@@ -4,14 +4,42 @@ namespace App\Exports;
 
 use App\Karyawan;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class KaryawanExport implements FromCollection
+class KaryawanExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Karyawan::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Nama Karyawan',
+            'Nomor Induk Kepegawaian',
+            'Tempat Lahir',
+            'Tanggal Lahir',
+            'Usia',
+            'Status Keluarga',
+            'Tanggal Masuk Kerja',
+            'Masa Kerja',
+            'No. Sk',
+            'Jabatan',
+            'Tanggal dipilih Jabatan',
+            'Masa Jabatan',
+            'Pendidikan',
+            'Jurusan',
+            'Nomor Induk Kependudukan',
+            'No. BPJS Ketenagakerjaan',
+            'No. BPJS Kesehatan',
+            'No. NPWP',
+            'created_at',
+            'updated_at',
+        ];
     }
 }
