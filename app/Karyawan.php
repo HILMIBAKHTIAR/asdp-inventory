e10649fce0fe71815405857d9b051f08b58e5560
Satria Teguh Perkasa <satriateguhghz@gmail.com>
2021-09-07 12:35:57 +0700

    add karyawan factory

--- app/Karyawan.php
+++ app/Karyawan.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Karyawan extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'nama_karyawan',
         'jabatan_id',
