24cd95627ac27820ed7bb59367962b5fabbb513e
samCZ054 <aramadhan39@gmail.com>
2021-09-09 10:36:58 +0700

    judul aplikasi update ASDP HR4U

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ASDP Procurement</title>
+    <title>ASDP HR4U</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
